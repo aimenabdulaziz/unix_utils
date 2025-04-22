bd2275ffe05af8f80e63e85c2a919ccd39d194cf
aimenabdulaziz <aimenabdulaziz@users.noreply.github.com>
2025-04-21 20:47:44 -0400

    feat: complete rhead

--- rhead/src/main.rs
+++ rhead/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, stdin};
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Display a line of text", long_about = None)]
@@ -8,29 +10,80 @@ struct Args {
         default_value_t = 10,
         help = "Print count lines of each of the specified files."
     )]
-    n: i32,
+    n: usize,
 
     // List of files
     files: Option<Vec<String>>,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    let num_lines = args.n;
-
-    println!("n: {}", args.n);
-    println!("{:?}", args.files);
+fn handle_error(e: io::Error, context: &str, error_code: i32) -> ! {
+    eprintln!("{}: {}", context, e);
+    std::process::exit(error_code);
 }
 
-// if there is no file, read from stdin
-// if there is a file, read from file
-// if there are multiple files, read from each file
-/*
-==> file name <==
-{file contents}
+// Only read the first n lines (good for large files cause the whole file is not loaded into memory)
+fn process_file(file_path: &str, num_lines: usize) -> io::Result<()> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
 
-==> file2.txt <==
+    println!("==> {file_path} <==");
 
-{file contents}.
-*/
+    // Read and print the first n lines
+    for (i, line) in reader.lines().enumerate() {
+        if i >= num_lines {
+            break;
+        }
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
+
+fn process_stdin(num_lines: usize) -> io::Result<()> {
+    // Start at 1 for each stdin session
+    let mut input: String = String::new();
+    let mut loaded: usize = 0;
+    loop {
+        match stdin().read_line(&mut input) {
+            Ok(0) => break, // EOF reached
+            Ok(_) => {
+                print!("{}", input);
+                input.clear();
+                loaded += 1;
+
+                // Break before reading more lines if we have reached the limit
+                if loaded >= num_lines {
+                    break;
+                }
+            }
+            Err(e) => return Err(e),
+        }
+    }
+    Ok(())
+}
+
+fn main() {
+    let args = Args::parse();
+    let num_lines: usize = args.n;
+
+    match args.files {
+        Some(files) => {
+            for (i, file) in files.iter().enumerate() {
+                if let Err(e) = process_file(&file, num_lines) {
+                    handle_error(e, "Error reading file", 1);
+                }
+
+                // Print a newline after each file, except for the last one (to match the behavior of `head`)
+                if i < files.len() - 1 {
+                    println!();
+                }
+            }
+        }
+        _ => {
+            // Read and print content from stdin if no files are given
+            if let Err(e) = process_stdin(num_lines) {
+                handle_error(e, "Error reading from stdin", 2);
+            }
+        }
+    }
+}
